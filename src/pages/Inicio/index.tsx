3fc4253e27bd687526c777e6cc054ee7532f275b
Kawan Marques <kawan15marques@gmail.com>
2022-12-19 18:20:30 -0300

    first commit

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,28 @@
+import cardapio from '../../Data/cardapio.json';
+import styles from './inicio.module.scss';
+
+export default function Inicio() {
+
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+
+  return (
+    <section>
+      <h3 className={styles.titulo}>
+        Recomendações da cozinha
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div key={item.id} className={styles.recomedado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+            <button className={styles.botao}> 
+              Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
